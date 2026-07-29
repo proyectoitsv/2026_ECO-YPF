8657cae17f16b01b86a1c80cec4c6d64262015d9
Martino <martino.righ@gmail.com>
2026-07-29 20:19:07 -0300

    Feat: Se modifica la medida de velocidad basandose en el tiempo entre cada pulso

--- Codigo_integrado/src/main.cpp
+++ Codigo_integrado/src/main.cpp
@@ -10,40 +10,39 @@ const float diametroRuedaMetros = 0.49;
 
 float tiempoSegundos1 = 0.0f; // Variable global para almacenar el tiempo transcurrido en segundos
 
-volatile unsigned long contadorPulsos = 0;
-unsigned long tiempoAnteriorInductivo = 0;
-const unsigned long intervaloInductivo = 1000; // Recálculo cada 1 segundo
-unsigned long pulsosCopiados = 0;
-float tiempoResta=0;
-
 float circunferencia = 3.141592 * diametroRuedaMetros;
 float rpm = 0.0;
 float velocidadKmH = 0.0;
 
 // ISR para el sensor inductivo
+int tiempoActual = 0;
+int tiempoInicio = 0;
+int tiempoGuardado = 0;
 void IRAM_ATTR cuentaPulsos() {
-    contadorPulsos++;
+    millis();
+    tiempoActual = millis();
+    tiempoGuardado = tiempoActual - tiempoInicio;
+    velocidadKmH = 1539.38 / tiempoGuardado; // (2*pi*r)/T
+    Serial.print(" | Velocidad: "); Serial.print(velocidadKmH, 2);
+    Serial.print(" km/h");
+    tiempoInicio = millis();
 }
 
 
-//ISR para el temporizador
-hw_timer_t *timer = NULL;
-volatile bool temporizadorListo = false;
-
-void IRAM_ATTR onTimer() {
-    temporizadorListo = true; 
-}
 
 // Objetos globales de temperatura
 GestorTemperatura sensorTemp1(25); // Primer sensor
 GestorTemperatura sensorTemp2(33); // Segundo sensor
 
+
+bool estadoPin = false;
 void setup() {
     Serial.begin(115200);
     
 
     // Configuración Batería
     pinMode(34, INPUT); // Pin analógico de la batería
+    
     
     // Configuración Temperatura
     sensorTemp1.inicializar();
@@ -53,22 +52,17 @@ void setup() {
     pinMode(pinSensorInductivo, INPUT);
     attachInterrupt(digitalPinToInterrupt(pinSensorInductivo), cuentaPulsos, RISING);
 
-    // Configuración del temporizador
-    timer = timerBegin(0, 80, true); //un tick cada 1 microsegundo
-    timerAttachInterrupt(timer, &onTimer, true);
-    timerAlarm(timer, 1000000, true); //alarma cada 1 segundo
-
 }
 
 void loop() {
     // 1. Lectura de Batería
     MedidaTension datos = leerTensionCompleta(); 
-
+/*
     Serial.print("Voltaje: "); Serial.print(datos.voltaje, 3);
     Serial.print(" V | ADC: "); Serial.print(datos.adc);
     Serial.print(" | Tensión Batería: "); Serial.print(datos.voltajeBateria, 2);
     Serial.print(" V | ");
-
+*/
     // 2. Lectura de Temperatura y Promedio
     sensorTemp1.solicitarTemperaturas();
     sensorTemp2.solicitarTemperaturas();
@@ -77,48 +71,10 @@ void loop() {
     float temp2 = sensorTemp2.leerTemperatura(0);
     float promedio = (temp1 + temp2) / 2.0;
     
-    Serial.print("Temp1: "); Serial.print(temp1);
+    /*Serial.print("Temp1: "); Serial.print(temp1);
     Serial.print(" °C | Temp2: "); Serial.print(temp2);
     Serial.print(" °C | ");
 
-
-    // 3. Temporizador 
-    if (temporizadorListo) { //adentro de este if pongan todo, yo puse lo que decia abajo para q guarde la variable pero siga contando
-        temporizadorListo = false; // Reinicia la bandera
-        unsigned long pulsosCopiados = contadorPulsos;
-        contadorPulsos = 0;
-        velocidadKmH = pulsosCopiados * circunferencia * 3.6; // Convertir RPM a km/h
-    }
-
-    /* 4. Lectura de Inductivo (Velocidad / RPM)
-    unsigned long tiempoActual = millis();
-    if (tiempoActual - tiempoAnteriorInductivo >= intervaloInductivo) {
-        
-        noInterrupts();
-        tiempoResta = tiempoActual - tiempoAnteriorInductivo;
-        
-        //unsigned long pulsosCopiados = contadorPulsos;
-        pulsosCopiados = contadorPulsos;
-        contadorPulsos = 0;
-        interrupts();
-
-        rpm = (pulsosCopiados * 60.0 * 1000) / (pulsosPorRevolucion * tiempoResta);
-        float circunferencia = 3.141592 * diametroRuedaMetros;
-        float rps = rpm / 60.0;
-        velocidadKmH = rps * circunferencia * 3.6;
-        
-
-        tiempoAnteriorInductivo = tiempoActual;
-        */
-    }
-   
-    Serial.println("Tiempo Resta"); Serial.print(tiempoResta, 3);
-    //Serial.println("RPM: "); Serial.print(rpm, 1);
-    Serial.print(" | Velocidad: "); Serial.print(velocidadKmH, 2);
-    Serial.print(" km/h");
-    
-    Serial.print(pulsosCopiados); 
-    Serial.println(" s");
-
+*/ 
     delay(100);
 }
