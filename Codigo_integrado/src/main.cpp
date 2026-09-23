12578d202bfc011c1e304bde3e93078e46d37411
tomas <carranzatomas73@gmail.com>
2026-09-22 23:18:31 -0300

    se eliminaron lineas de codigo inecesarias

--- Codigo_integrado/src/main.cpp
+++ Codigo_integrado/src/main.cpp
@@ -2,10 +2,8 @@
 //EL CODIGO ESTE FUE PROBADO EN UNA ESP32C3, POR LO TANTO SE DEBEN DE CAMBIAR LOS PINES Y LA CONFIGURACION DEL .INI
 //------------------------------------------------------------------------------------------------------------------
 #include <Arduino.h>
-#include "Sensores/Tension/Tension.h"
-#include "Sensores/Temperatura/Temperatura.h"
-#include "Sensores/Inductivo(velocidad)/Inductivo.h"
-
+#include <SPI.h>
+#include <Wire.h>
 void setup() {
 
 }
